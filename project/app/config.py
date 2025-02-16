c486d6f2e73ac602ada12b8cf0340e0eb353ddab
stephentyers1975 <steve.tyers@gmail.com>
2025-02-16 14:08:30 +0000

    Created postgress DB and tortoise ORM

--- project/app/config.py
+++ project/app/config.py
@@ -3,6 +3,8 @@
 
 import logging
 from functools import lru_cache
+
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 
@@ -12,6 +14,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = bool(0)
+    database_url: AnyUrl = None
 
 
 @lru_cache()
