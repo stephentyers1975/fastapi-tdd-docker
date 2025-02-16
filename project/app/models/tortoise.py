ace319acd60f136c71d6cdc5e5eb3e6d767f3ecc
stephentyers1975 <steve.tyers@gmail.com>
2025-02-16 15:14:02 +0000

    Created Restful Routes with tests

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,6 +1,8 @@
 # project/app/models/tortoise.py
 
+
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator  # new
 
 
 class TextSummary(models.Model):
@@ -10,3 +12,6 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)  # new
