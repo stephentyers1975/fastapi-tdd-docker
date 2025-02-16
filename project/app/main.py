e2df783b4e232175ea181bf4acd82d69d7a003f9
stephentyers1975 <steve.tyers@gmail.com>
2025-02-16 16:26:02 +0000

    Revert "Created Restful Routes with tests"
    This reverts commit ace319acd60f136c71d6cdc5e5eb3e6d767f3ecc.

--- project/app/main.py
+++ project/app/main.py
@@ -1,34 +1,25 @@
 # project/app/main.py
 
-
-import logging
-
-from fastapi import FastAPI
-
-from app.api import ping, summaries  # updated
-from app.db import init_db
+import os
+from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
+from app.config import get_settings, Settings
 
 
-log = logging.getLogger("uvicorn")
+app = FastAPI()
 
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,  # updated
+    add_exception_handlers=True,
+)
 
-def create_application() -> FastAPI:
-    application = FastAPI()
-    application.include_router(ping.router)
-    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
-
-    return application
-
-
-app = create_application()
-
-
-@app.on_event("startup")
-async def startup_event():
-    log.info("Starting up...")
-    init_db(app)
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    log.info("Shutting down...")
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,8 +1,6 @@
 # project/app/models/tortoise.py
 
-
 from tortoise import fields, models
-from tortoise.contrib.pydantic import pydantic_model_creator  # new
 
 
 class TextSummary(models.Model):
@@ -12,6 +10,3 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
-
-
-SummarySchema = pydantic_model_creator(TextSummary)  # new
